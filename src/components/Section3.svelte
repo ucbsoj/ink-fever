e8432d569ac1757a95c61994f532fd58cd00ef99
wakeupxavey <xavey.bzdek@berkeley.edu>
2025-04-25 15:10:35 -0700

    Holy mama we did a big ol svg and coded the pop ups to cordniate to each rect id, located in the body1 svg code, congrats this was a LOT

--- src/components/Section3.svelte
+++ src/components/Section3.svelte
@@ -1,6 +1,7 @@
 
 <script>
     import Skull from "$components/Skull.svelte";
+	import bodySvg from "$svg/body1.svg";
 
 	let buttons = [
 		{text: "A", left: "50%", top: "50%"},
@@ -18,42 +19,71 @@
 		buttonPressed = undefined;
 	}
 
-	$inspect({buttonPressed})
+	$effect(() => {
+		// Get all the elipses in the SVG and run onclick when clicked
+		// const elipses = document.querySelectorAll("ellipse");
+		// elipses.forEach((ellipse) => {
+		// 	ellipse.addEventListener("click", () => {
+		// 		const buttonId = ellipse.id;
+		// 		onClick(buttonId);
+
+		// get all rect in the SVG and run onclick when clicked
+		const rects = document.querySelectorAll("rect");
+		rects.forEach((rect) => {
+			rect.addEventListener("click", () => {
+				const buttonId = rect.id;
+				onClick(buttonId);
+			
+			});
+		});
+	});
+
+	$inspect(buttonPressed)
 </script>
-<div class="end">
+
+<!-- <div class="end"> -->
 
 
 
-<div class="body-map">
+<!-- <div class="body-map">
 	{#each buttons as button}
 		<button class="tattoo" style:left={button.left} style:top={button.top} onclick={() => onClick(button.text)}>
 			<img src="assets/images/icon3.png"/>
-             
+				
 		</button>
 	{/each}
-</div>
+</div> -->
 
-<div class="popup" class:visible={buttonPressed !== undefined}>
-		{#if buttonPressed === "A"}
+<div class="end">
+	<div class="bodSvg">
+
+		{@html bodySvg}
+	</div>
+
+
+
+	<div class="popup" class:visible={buttonPressed !== undefined}>
+		{#if buttonPressed === "skull"}
 			<Skull />
-		{:else if buttonPressed === "B"}
-			<p>This is the B content</p>
+		{:else if buttonPressed === "ball"}
+			<p>This is a modern history of Tattoos</p>
 		{:else}
 			<p>You clicked something other than A or B!</p>
 		{/if}
 
-	<button class="close" onclick={close}>x</button>
+		<button class="close" onclick={close}>x</button>
+	</div>
+
 </div>
 
-
-</div>
+<!-- </div> -->
 <!-- closed .end -->
 
 <style>
 	.popup {
 		background: #faefe0;
 		height: 400px;
-		width: 400px;
+		width: 600px;
 		position: absolute;
 		top: 50%;
 		left: 50%;
@@ -76,7 +106,7 @@
 	button.tattoo {
 		position: absolute;
 		border: grey;
-		background: #F9E4C6;
+		background:transparent;
         width: 50px;
         height: 50px;
         border-radius: 50%;
@@ -99,11 +129,27 @@
         height: 100vh;
         width: 100vw;
         position: relative;
-        background-image: url("assets/images/tattoomap.png");
+        /* background-image: url("assets/images/tattoomap.png"); */
         background-size: contain;
         background-position: center;
         background-repeat: no-repeat;
+		margin: 5px, 10px;
+		padding: 2px, 2px;
+		justify-content: center;
         
     }
+
+	.bodSvg {
+		width: 400px;
+		height: 800px;
+		position: relative;
+		background: transparent;
+		top: 50vh;
+		left: 40vw;
+		transform: translate(-50%, -50%);
+		padding: 2rem;
+		
+	}
+		
 
 </style>
